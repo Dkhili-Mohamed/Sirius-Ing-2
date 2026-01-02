ab56df2a78a1d0e8e96d5dd8cf2fcea7ac32efca
SOUPGUI Maxime <maximesoupgui@gmail.com>
2026-01-02 11:43:51 +0100

    Refactoring construction du graphe.

--- back/src/main/java/esiag/back/services/CheminService.java
+++ back/src/main/java/esiag/back/services/CheminService.java
@@ -3,6 +3,7 @@ package esiag.back.services;
 
 import esiag.back.models.architecture.Connexion;
 import esiag.back.repositories.ConnexionRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,22 +14,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Log4j2
 @Service
 public class CheminService {
 
     @Autowired
     private ConnexionRepository connexionRepository;
 
-    public Map<Long, List<Long>> grapheDuPlan;
 
     public CheminService(ConnexionRepository connexionRepository) {
         this.connexionRepository = connexionRepository;
     }
 
-    @PostConstruct
-    public Map<Long, List<Long>> contructionGrapheDuPlan(){
+    public Map<Long, List<Long>> getGrapheDuPlan(){
+
         List<Connexion> connexions = connexionRepository.findAll();
-        grapheDuPlan = new HashMap<>();
+        log.info("Lecture des connexions");
+
+        Map<Long, List<Long>> grapheDuPlan = new HashMap<>();
 
         for (Connexion connexion : connexions) {
             Long sommet1 = connexion.getIdEspace1();
@@ -42,11 +45,8 @@ public class CheminService {
             grapheDuPlan.get(sommet2).add(sommet1);
 
         }
+        log.info("Construction du graphe terminé");
         return grapheDuPlan;
-    }
-
-    public Map<Long, List<Long>> getGrapheDuPlan() {
-        return contructionGrapheDuPlan();
     }
 
 
