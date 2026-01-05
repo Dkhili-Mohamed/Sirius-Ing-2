474140c41cfcb0c6ec21daa43841c1f982165def
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-01-05 23:28:18 +0100

    Ajout de PatientDataLoader.java pour remplir la table Patient

---
+++ back/src/main/java/esiag/back/loaders/PatientDataLoader.java
@@ -0,0 +1,44 @@
+package esiag.back.loaders;
+
+import esiag.back.models.medical.Patient;
+import esiag.back.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.CommandLineRunner;
+
+@Component
+public class PatientDataLoader implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Patient p1 = new Patient();
+        p1.setNomPatient("Dupont");
+        p1.setPrenomPatient("Jean");
+        p1.setAgePatient(30);
+
+        Patient p2 = new Patient();
+        p2.setNomPatient("Martin");
+        p2.setPrenomPatient("Claire");
+        p2.setAgePatient(25);
+
+        Patient p3 = new Patient();
+        p3.setNomPatient("Durand");
+        p3.setPrenomPatient("Luc");
+        p3.setAgePatient(40);
+
+        patientRepository.save(p1);
+        System.out.println("Patient " + p1.getNomPatient() + " " + p1.getPrenomPatient() + " ajouté à la table Patients !");
+        patientRepository.save(p2);
+        System.out.println("Patient " + p2.getNomPatient() + " " + p2.getPrenomPatient() + " ajouté à la table Patients !");
+        patientRepository.save(p3);
+        System.out.println("Patient " + p3.getNomPatient() + " " + p3.getPrenomPatient() + " ajouté à la table Patients !");
+
+
+
+    }
+
+}
