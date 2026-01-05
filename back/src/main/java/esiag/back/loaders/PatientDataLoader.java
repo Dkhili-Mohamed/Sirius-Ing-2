c684d164ba26c3f67f3bd0ff5a123cb05a1ea62a
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-01-05 23:44:09 +0100

    Gestion des doublons implémentée

--- back/src/main/java/esiag/back/loaders/PatientDataLoader.java
+++ back/src/main/java/esiag/back/loaders/PatientDataLoader.java
@@ -15,28 +15,36 @@ public class PatientDataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Patient p1 = new Patient();
-        p1.setNomPatient("Dupont");
-        p1.setPrenomPatient("Jean");
-        p1.setAgePatient(30);
+        if(patientRepository.count() == 0) {
 
-        Patient p2 = new Patient();
-        p2.setNomPatient("Martin");
-        p2.setPrenomPatient("Claire");
-        p2.setAgePatient(25);
+            Patient p1 = new Patient();
+            p1.setNomPatient("Dupont");
+            p1.setPrenomPatient("Jean");
+            p1.setAgePatient(30);
 
-        Patient p3 = new Patient();
-        p3.setNomPatient("Durand");
-        p3.setPrenomPatient("Luc");
-        p3.setAgePatient(40);
+            Patient p2 = new Patient();
+            p2.setNomPatient("Martin");
+            p2.setPrenomPatient("Claire");
+            p2.setAgePatient(25);
 
-        patientRepository.save(p1);
-        System.out.println("Patient " + p1.getNomPatient() + " " + p1.getPrenomPatient() + " ajouté à la table Patients !");
-        patientRepository.save(p2);
-        System.out.println("Patient " + p2.getNomPatient() + " " + p2.getPrenomPatient() + " ajouté à la table Patients !");
-        patientRepository.save(p3);
-        System.out.println("Patient " + p3.getNomPatient() + " " + p3.getPrenomPatient() + " ajouté à la table Patients !");
+            Patient p3 = new Patient();
+            p3.setNomPatient("Durand");
+            p3.setPrenomPatient("Luc");
+            p3.setAgePatient(40);
 
+            patientRepository.save(p1);
+            System.out.println("Patient " + p1.getNomPatient() + " " + p1.getPrenomPatient() + " ajouté à la table Patients !");
+            patientRepository.save(p2);
+            System.out.println("Patient " + p2.getNomPatient() + " " + p2.getPrenomPatient() + " ajouté à la table Patients !");
+            patientRepository.save(p3);
+            System.out.println("Patient " + p3.getNomPatient() + " " + p3.getPrenomPatient() + " ajouté à la table Patients !");
+
+        }
+
+        else {
+            System.out.println("Patients déjà ajoutés ou aucun nouveau patient à ajouter.");
+
+        }
 
 
     }
