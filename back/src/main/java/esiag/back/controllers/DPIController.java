4580935e4b8a22bef32d8916b1158361226ab6bf
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-01-07 04:17:01 +0100

    Création des classes nécessaires au dpi

---
+++ back/src/main/java/esiag/back/controllers/DPIController.java
@@ -0,0 +1,55 @@
+package esiag.back.controllers;
+
+import esiag.back.models.medical.DPI;
+import esiag.back.services.DPIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/DPI")
+public class DPIController {
+
+    @Autowired
+    private DPIService dpiService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DPI> findById(@PathVariable Long id){
+
+        return new ResponseEntity<>(dpiService.findByIdDPI(id), HttpStatus.OK);
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<DPI>> findAllDPI(){
+        return new ResponseEntity<>(dpiService.findAllDPIs(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DPI> updateDPI(@PathVariable Long id, @RequestBody DPI dpiUpdates) {
+
+        DPI dpiExistant = dpiService.findByIdDPI(id);
+
+        if(dpiExistant == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        dpiExistant.setAntecedent(dpiUpdates.getAntecedent());
+        dpiExistant.setTraitement(dpiUpdates.getTraitement());
+
+        dpiService.saveDPI(dpiExistant);
+
+        return  new ResponseEntity<>(dpiExistant, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> deleteMapping(@PathVariable Long id){
+        boolean isRemoved = dpiService.deleteDPI(id);
+        if(!isRemoved){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return  new ResponseEntity<>(id, HttpStatus.OK);
+    }
+}
--- back/src/main/java/esiag/back/repositories/MaladiePatientRepository.java
+++ back/src/main/java/esiag/back/repositories/MaladiePatientRepository.java
@@ -1,10 +1,10 @@
 package esiag.back.repositories;
 
-import esiag.back.models.medical.Patient;
+import esiag.back.models.medical.MaladiePatient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
+public interface MaladiePatientRepository extends JpaRepository<MaladiePatient, Long> {
 
 }
