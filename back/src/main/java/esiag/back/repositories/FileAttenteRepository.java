ae23f50985a740c35a92c80c7c0b0a68e43fd3b5
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-01-18 18:30:22 +0100

    Ajouts et modifications

---
+++ back/src/main/java/esiag/back/repositories/FileAttenteRepository.java
@@ -0,0 +1,13 @@
+package esiag.back.repositories;
+
+import esiag.back.models.medical.FileAttente;
+import esiag.back.models.medical.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FileAttenteRepository extends JpaRepository<FileAttente, Long> {
+    boolean existsByPatient (Patient patient);
+    void deleteByPatient (Patient patient);
+
+}
