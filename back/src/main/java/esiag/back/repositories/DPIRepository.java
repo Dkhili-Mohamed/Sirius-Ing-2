37096d4ea0f2d504bcdcb83100c414aa985eb04e
SOUPGUI Maxime <maximesoupgui@gmail.com>
2026-01-21 22:15:20 +0100

    Correction du type d'identifiant dans DPIRepository pour utiliser Integer au lieu de Long

--- back/src/main/java/esiag/back/repositories/DPIRepository.java
+++ back/src/main/java/esiag/back/repositories/DPIRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DPIRepository extends JpaRepository<DPI, Long> {
+public interface DPIRepository extends JpaRepository<DPI, Integer> {
 
 }
