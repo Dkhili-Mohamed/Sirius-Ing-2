4c98a1a2e8c48eeac02896c65343bab9c74d76f5
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-01-21 01:04:37 +0100

    Test de la file d'attente et affichage dans le backend des logs avec TestFileAttente.java

---
+++ back/src/main/java/esiag/back/TestFileAttente.java
@@ -0,0 +1,41 @@
+package esiag.back;
+
+import esiag.back.models.medical.FileAttente;  // Ajoutez cet import
+import esiag.back.models.medical.Patient;
+import esiag.back.services.FileAttenteService;
+import esiag.back.services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestFileAttente implements CommandLineRunner {
+
+    @Autowired
+    private FileAttenteService fileAttenteService;
+
+    @Autowired
+    private PatientService patientService;
+
+    @Override
+    public void run(String... args) {
+        System.out.println("=== DÉBUT DU TEST FILE D'ATTENTE ===");
+
+        System.out.println("\nListe de tous les patients :");
+        for (Patient patient : patientService.findAllPatients()) {
+            System.out.println("- " + patient.getPrenomPatient() + " " +
+                    patient.getNomPatient() + " " + patient.getAgePatient() + " ans" +
+                    " (Urgence: " + patient.getScoreUrgence() + ")");
+        }
+
+        System.out.println("\nFile d'attente triée par urgence :");
+        for (FileAttente fileAttente : fileAttenteService.getFileAttenteTriee()) {
+            Patient patient = fileAttente.getPatient();
+            System.out.println("- " + patient.getPrenomPatient() + " " +
+                    patient.getNomPatient() + " " + patient.getAgePatient() + " ans" +
+                    " (Urgence: " + patient.getScoreUrgence() + ")");
+        }
+
+        System.out.println("=== FIN DU TEST ===");
+    }
+}
