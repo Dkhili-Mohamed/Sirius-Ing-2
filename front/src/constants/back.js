8c207a7e1190dc80d5732188fe7b9273604f33ce
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-04-01 23:38:48 +0200

    Modification des routes et adresses ip

--- front/src/constants/back.js
+++ front/src/constants/back.js
@@ -6,7 +6,8 @@ export const UPDATE_AMBULANCE = LOCAL_HOST_AMBULANCE + 'update';
 //export const LOCAL_HOST = 'http://172.31.249.73:8081';
 
 //Table Patients +
-export const LOCAL_HOST = 'http://172.31.249.30:8081';
+//export const LOCAL_HOST = 'http://172.31.249.30:8081';
+export const LOCAL_HOST = 'http://localhost:8081';
 export const LOCAL_HOST_PATIENT = LOCAL_HOST + '/api/patient/';
 export const GET_PATIENTS = LOCAL_HOST_PATIENT + 'all';
 export const UPDATE_PATIENT = LOCAL_HOST_PATIENT + 'update';
@@ -22,6 +23,8 @@ export const INSERT_PARCOURS = LOCAL_HOST + '/api/parcours/insert';
 export const INSERT_SUIVRE = LOCAL_HOST + '/api/suivre/insert';
 export const SYMPTOME_PATIENT = LOCAL_HOST + '/api/symptome/';
 export const BOX_MEDICALE = LOCAL_HOST + '/api/box-medicale';
+export const LIBERER_BOX = LOCAL_HOST + '/api/box-medicale/liberer';
+
 
 
 
