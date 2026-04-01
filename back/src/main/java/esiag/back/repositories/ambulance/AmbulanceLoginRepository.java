6e1eba51762f42255ab2fb9962281a9bb07fb083
Dkhili-Mohamed <raslendkhili76@gmail.com>
2026-04-01 16:12:09 +0100

    Update AmbulanceLoginRepository.java

--- back/src/main/java/esiag/back/repositories/ambulance/AmbulanceLoginRepository.java
+++ back/src/main/java/esiag/back/repositories/ambulance/AmbulanceLoginRepository.java
@@ -1,4 +1,4 @@
-package esiag.back.repositories;
+package esiag.back.repositories.ambulance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
