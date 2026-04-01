fcd6da6f4d2f0d3f7125c1c834746e79786e800c
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-04-01 23:33:42 +0200

    Ajout de la méthode pour libérer manuellement une box

--- back/src/main/java/esiag/back/controllers/fileattente/BoxMedicaleController.java
+++ back/src/main/java/esiag/back/controllers/fileattente/BoxMedicaleController.java
@@ -2,6 +2,7 @@ package esiag.back.controllers.fileattente;
 
 import esiag.back.models.medical.BoxMedicale;
 import esiag.back.services.fileattente.BoxMedicaleService;
+import esiag.back.services.fileattente.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,8 @@ public class BoxMedicaleController {
 
     @Autowired
     private BoxMedicaleService boxMedicaleService;
+    @Autowired
+    private PatientService patientService;
 
     @GetMapping
     public ResponseEntity<List<BoxMedicale>> getAllBoxMedicale() {
@@ -30,6 +33,17 @@ public class BoxMedicaleController {
             return ResponseEntity.ok(boxMedicales);
 
 
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PostMapping ("liberer/{idBoxMedicale}/{idPatient}")//pour supprimer un patient de la box, avec un appel a liberer box
+    public ResponseEntity<Long> libererPatientBox(@PathVariable Long idBoxMedicale, @PathVariable Long idPatient) {
+        try{
+            boxMedicaleService.libererBox(idBoxMedicale);
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
