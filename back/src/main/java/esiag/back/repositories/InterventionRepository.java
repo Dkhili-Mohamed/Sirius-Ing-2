9644a710aaa403a6bb5823b602319f5a232254d3
Dkhili-Mohamed <raslendkhili76@gmail.com>
2026-04-02 10:41:53 +0100

    j'ai reglé un probleme dans l'historique des interventions et jai rajouté des prenoms pour les patients

--- back/src/main/java/esiag/back/repositories/InterventionRepository.java
+++ back/src/main/java/esiag/back/repositories/InterventionRepository.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import esiag.back.models.ambulance.Intervention;
 
+
 @Repository
 public interface InterventionRepository extends JpaRepository<Intervention, Long> {
     List<Intervention> findAllByOrderByDateinterventionDesc();
+    // Vérifie si une intervention avec une adresse et un statut spécifiques existe déjà pour eviter d'ajouter plusieurs fois la meme intervention dans l'historique
+    boolean existsByAdresseinterventionAndInterventionstatut(String adresse, String statut);
 }
