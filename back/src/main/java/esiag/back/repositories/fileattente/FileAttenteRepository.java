a96fbb21070a7a087ae50dfa18fb9a29661bd000
Shaina AGATHINE <shaina.agathine@etu.u-pec.fr>
2026-04-29 00:02:04 +0200

    Suppression des requêtes incorrectes du repository

--- back/src/main/java/esiag/back/repositories/fileattente/FileAttenteRepository.java
+++ back/src/main/java/esiag/back/repositories/fileattente/FileAttenteRepository.java
@@ -12,14 +12,5 @@ public interface FileAttenteRepository extends JpaRepository<FileAttente, Long> 
     @Query("SELECT COUNT(fa) FROM FileAttente fa")
     long countNombrePatients();
 
-    @Query("SELECT COUNT(fa) FROM FileAttente fa WHERE fa.patient.statutPatient = 'URGENT' ")
-    long countNombrePatientsUrgents();
-
-    @Query("SELECT COUNT(fa) FROM FileAttente fa WHERE fa.patient.statutPatient = 'INTERMEDIAIRE' ")
-    long countNombrePatientsIntermediaires();
-
-    @Query("SELECT COUNT(fa) FROM FileAttente fa WHERE fa.patient.statutPatient = 'NON_URGENT' ")
-    long countNombrePatientsNonUrgents();
-
 
 }
